897dd04a3e1d9dcffc7232f8a8e4f0f6dbd2d17f
efsef <inoplanee@mail.ru>
2019-05-26 12:37:41 +0600

    Initial commit

---
+++ app/src/main/java/mainactivity/musicplayer/example/com/peger/MainActivity.java
@@ -0,0 +1,13 @@
+package mainactivity.musicplayer.example.com.peger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
